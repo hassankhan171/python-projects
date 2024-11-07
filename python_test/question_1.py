aa0c8a0316e428445a7e119f6016b9b6758f386a
hassankhan171 <hassankhan7702@gmail.com>
2024-11-07 17:58:35 +0500

    modified

--- python_test/question_1.py
+++ python_test/question_1.py
@@ -1,28 +1,31 @@
-
 def create_unique_words():
-        try:
-            n = int(input("Enter the number of words: "))
-            words = []
-            for i in range(1, n+1):
-                word = input(f"Enter the word {i}: ").lower()
-                words.append(word)
-            unique_words = set(words)
-            print("Unique words:", unique_words)
-            print("Total unique words:", len(unique_words))
+    try:
+        n = int(input("Enter the number of words: "))
+        print("--------------------------------")
+        words = []
+        for i in range(1, n + 1):
+            word = input(f"Enter the word {i}: ").lower()
+            words.append(word)
+        unique_words = set(words)
+        print("--------------------------------")
+        print("Unique words: ", unique_words)
+        print("Total unique words: ", len(unique_words))
+        print("--------------------------------")
 
-            word_count = {}   # Empty Dictionary
-            for word in words:
-                if word in word_count:
-                    word_count[word] += 1
-                else:
-                    word_count[word] = 1
+        word_count = {}  # Empty Dictionary
+        for a in words:
+            if a in word_count:
+                word_count[a] += 1
+            else:
+                word_count[a] = 1
 
-            for word, count in word_count.items():
-                print(f"{word}: {count}")
+        for a, count in word_count.items():
+            print(f"{a}: {count}")
 
-            return word_count
-        except Exception as e:
-            print('Something went wrong')
-            print('Error reason: ', e)
+        return word_count
+    except Exception as e:
+        print('Something went wrong')
+        print('Error reason: ', e)
+
 
 create_unique_words()
