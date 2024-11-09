07406358fe86e79580833a684d0ee39965458d67
hassankhan171 <hassankhan7702@gmail.com>
2024-11-10 00:01:25 +0500

    Even and Odd

---
+++ python_practice_problems/Question10.py
@@ -0,0 +1,23 @@
+#You are going to be given a word. Your job is to return the middle character of the
+#word. If the word's length is odd, return the middle character. If the word's length is
+#even, return the middle 2 characters.
+#test => es
+#testing => t
+
+word = input("Enter The Word: ")
+n = len(word)
+print("----------")
+print(f"Total {n} characters")
+if n%2 == 0:
+    print("It is an Even number")
+    print("----------")
+    print(word[n//2 - 1], word[n//2])
+else:
+    print("It is an Odd number")
+    print("----------")
+    print(word[(n-1)//2])
+
+
+
+
+
