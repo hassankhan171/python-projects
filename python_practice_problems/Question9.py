cd4887fae044dbcd07d1c18a3a85feadfe733d74
hassankhan171 <hassankhan7702@gmail.com>
2024-11-12 19:59:55 +0500

    modified

--- python_practice_problems/Question9.py
+++ python_practice_problems/Question9.py
@@ -7,11 +7,14 @@ string. All spaces in the string should be retained.
 
 # solution
 
-word = input("Enter the words:  ")
-n = len(word)
-a = []
-for i in range(n-1, -1, -1):
-    a.append(word[i])
-print("".join(a))
-
+try:
+    word = input("Enter the words:  ")
+    n = len(word)
+    a = []
+    for i in range(n-1, -1, -1):
+        a.append(word[i])
+    print("".join(a))
+except Exception as e:
+    print('Something went wrong')
+    print('Error reason: ', e)
 
